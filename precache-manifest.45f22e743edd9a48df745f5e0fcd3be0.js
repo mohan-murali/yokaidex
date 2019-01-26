e12140e9f4fc8ed4bffba43df610bf6e4e776ff0
joaopedrodcf <joaoferr93@gmail.com>
2019-01-26 15:36:33 +0000

    Updates

---
+++ precache-manifest.45f22e743edd9a48df745f5e0fcd3be0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "82f4b94863aa7160c4a6",
+    "url": "/yokaidex/static/js/runtime~main.82f4b948.js"
+  },
+  {
+    "revision": "93fb543c7efa8a274730",
+    "url": "/yokaidex/static/js/main.93fb543c.chunk.js"
+  },
+  {
+    "revision": "2282d6cb6d42f026349e",
+    "url": "/yokaidex/static/js/1.2282d6cb.chunk.js"
+  },
+  {
+    "revision": "bd9a1e068161b02afdd907ec63c31ec7",
+    "url": "/yokaidex/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/yokaidex/precache-manifest.229a0f9c1116c81581c127e7e338582b.js"
+  "/yokaidex/precache-manifest.45f22e743edd9a48df745f5e0fcd3be0.js"
 );
 
 workbox.clientsClaim();
